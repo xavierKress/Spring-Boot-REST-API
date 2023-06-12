d6bf7e599d13e6e26d8f0a628d7d74466e96b72e
Xavier Kress <xkress@mediametrie.fr>
2023-06-12 08:56:58 +0200

    Add the security layer

--- src/main/java/com/example/spring_boot_rest_api/api/v1/BookRestController.java
+++ src/main/java/com/example/spring_boot_rest_api/api/v1/BookRestController.java
@@ -1,7 +1,5 @@
 package com.example.spring_boot_rest_api.api.v1;
 
-
-
 import com.example.spring_boot_rest_api.model.Book;
 import com.example.spring_boot_rest_api.repository.BookRepository;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,9 +11,16 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
+@EnableMethodSecurity
 @RequestMapping("/api/v1/books")
 @Tag(name = "Book", description = "Book management APIs")
 public class BookRestController {
@@ -30,23 +35,27 @@ public class BookRestController {
 			description = "Get the list of books. The response is a collection of Book object with id, title, ISBN authors and publisher.",
 			tags = { "books", "get" })
 	@ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = Book.class), mediaType = "application/json") })
+	@PreAuthorize("hasAuthority('CAN_VIEW_BOOK_LIST')")
 	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public ResponseEntity<Iterable<Book>> getBooks(){
 		return new ResponseEntity<>(bookRepository.findAll(), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasAuthority('CAN_VIEW_BOOK_DETAIL')")
 	@GetMapping(value="/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public ResponseEntity<Book> getBookById(@PathVariable("id") Long id){
 		Book book = bookRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
 		return new ResponseEntity<>(book, HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasAuthority('CAN_ADD_BOOK')")
 	@PostMapping(consumes= MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.CREATED)
 	public Book addBook(@RequestBody Book book) {
 		return bookRepository.save(book);
 	}
 
+	@PreAuthorize("hasAuthority('CAN_UPDATE_BOOK')")
 	@PatchMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Book patchBookTitle(@PathVariable("id") Long id, @RequestBody Book patch){
 		Book bookToPatch = bookRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
@@ -54,16 +63,30 @@ public class BookRestController {
 		return bookRepository.save(bookToPatch);
 	}
 
+	@PreAuthorize("hasAuthority('CAN_DELETE_BOOK')")
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteBook(@PathVariable("id") Long id) {
 		bookRepository.deleteById(id);
 	}
 
+	@GetMapping("/securedPageWithPrincipalInfos")
+	public ResponseEntity<String> secured() {
+		return new ResponseEntity<>( "Secured authenticated page, Hello " + getName(), HttpStatus.OK);
+	}
+
 	@ExceptionHandler({EntityNotFoundException.class})
 	public ResponseEntity<String> bookNotFound() {
-
 		return new ResponseEntity<>("book not found", HttpStatus.NOT_FOUND);
+	}
+
+	private String getName() {
+		var authentication = SecurityContextHolder.getContext().getAuthentication();
+		return Optional.of(authentication.getPrincipal())
+				.filter(OidcUser.class::isInstance)
+				.map(OidcUser.class::cast)
+				.map(OidcUser::getEmail)
+				.orElseGet(authentication::getName);
 	}
 
 }
